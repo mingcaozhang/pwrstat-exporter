84838ee09db619698c5838e805fbb9eaeb7a5afe
root <root@proxmox.local>
2021-03-01 00:27:08 -0500

    Wait for daemon before pwrstat config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,11 +3,11 @@ set -e
 
 if [ "$1" = 'pwrstat-exporter' ]; then
   pwrstatd &
+  sleep 5 # wait for the daemon to be up
   pwrstat -pwrfail -shutdown off
   pwrstat -pwrfail -active off
   pwrstat -lowbatt -shutdown off
   pwrstat -lowbatt -active off
-  pwrstat -config
 fi
 
 exec "$@"
